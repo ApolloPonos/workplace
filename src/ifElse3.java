84e2b156544ecf2917f41915d01dc2aa37991c64
apolloponos <apolloponos@gmail.com>
2016-07-11 23:37:01 +0300

    ornek degisiklik4

---
+++ src/ifElse3.java
@@ -0,0 +1,13 @@
+
+public class ifElse3 {
+
+
+	public static void main(String[] args) {
+		 int parola=2013;
+
+	        if (parola==2013) {
+	            System.out.println("Parolalar eşleşiyor.");
+	        }
+
+	    }
+	}
---
+++ src/ifElse4.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ifElse4 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner giris=new Scanner(System.in);
+
+        
+        int istanbulPlakaKodu=34;
+        int girilenPlakaKodu;
+
+      
+        System.out.println("Istanbul'un plaka kodu nedir?");
+
+       
+        girilenPlakaKodu=giris.nextInt();
+
+        if(girilenPlakaKodu==istanbulPlakaKodu) {
+            System.out.println("Evet! Dogru yanit.");
+        } else {
+            System.out.println("Hayir, Istanbul'un plaka kodu 34'tur.");
+        
+
+    }
+    }
+}
+
