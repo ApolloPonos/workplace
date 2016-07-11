d408cbe86f7dbcd31dc3bd2a7653326185ae1c37
apolloponos <apolloponos@gmail.com>
2016-07-11 23:29:59 +0300

    ornek degisiklik3

---
+++ src/forDongusu3.java
@@ -0,0 +1,15 @@
+public class forDongusu3 {
+
+
+	public static void main(String[] args) {
+		int a, i;
+		for (a = 1; a <= 5; a++) {
+			System.out.println("a= " + a);
+			for (i = 1; i <= 3; i++) {
+				System.out.println("i= " + i);
+			}
+			System.out.println();
+		}
+	}
+
+}
