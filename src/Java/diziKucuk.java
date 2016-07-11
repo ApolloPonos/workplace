f515526eab9668521a534ce68d6b212ec37ec09c
apolloponos <apolloponos@gmail.com>
2016-07-11 20:34:23 +0300

    ornek degisiklik

--- src/Java/diziKucuk.java
+++ src/Java/diziKucuk.java
@@ -5,6 +5,7 @@ public class diziKucuk {
 	public static void main(String[]args){
 		int i, size=10;
 		int k;
+		//Ornek degisiklik
 		
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("enter an integer");
