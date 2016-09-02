5d94de7b2aab68ef03f0ecc9868bcdd3f7592655
apolloponos <apolloponos@gmail.com>
2016-09-02 22:06:07 +0300

    ‘H’

--- Ornek/edu/java/puzzle/ifElse7.java
+++ Ornek/edu/java/puzzle/ifElse7.java
@@ -1,12 +1,34 @@
 package edu.java.puzzle;
 
+import java.util.Scanner;
+
 public class ifElse7 {
 
-	/**
-	 * @param args
-	 */
+	private static final int GECME_NOTU = 45;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner notAl = new Scanner(System.in);
+		System.out.printf("Notunuzu girin");
+		int not = notAl.nextInt();
+		if (not < GECME_NOTU) {
+		
+			System.out
+					.printf("Bu dersten kaldınız. Daha çok çalışmanız gerekiyor");
+	
+		} 
+		else {
+			System.out.printf("Bu dersten geçtiniz. Tebrikler");
+			if (not <= 60) {
+				System.out.printf("Notunuz DD");
+			} else if (not <= 70) {
+				System.out.printf("Notunuz CC");
+			} else if (not <= 85) {
+				System.out.printf("Notunuz BB");
+			} else {
+				System.out.printf("Notunuz AA");
+			}
+		}
 
 	}
 
---
+++ Ornek/edu/java/puzzle/ifElse8.java
@@ -0,0 +1,24 @@
+package edu.java.puzzle;
+
+public class ifElse8 {
+
+	
+	public static void main(String[] args) {
+		int puan = 66;
+
+		if (puan < 25)
+			System.out.println("0");
+		else if (puan < 45)
+			System.out.println("1");
+		else if (puan < 55)
+			System.out.println("2");
+		else if (puan < 70)
+			System.out.println("3");
+		else if (puan < 85)
+			System.out.println("4");
+		else
+			System.out.println("5");
+
+	}
+
+}
