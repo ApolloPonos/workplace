367ea3360cc5d1cb5d823dce73d0d8f22fbbbd72
apollo <apolloponos@gmail.com>
2017-04-13 17:08:18 +0300

    parametrized

--- Ornek/edu/java/puzzle/top.java
+++ Ornek/edu/java/puzzle/top.java
@@ -2,27 +2,27 @@ package edu.java.puzzle;
 
 public class top {
 	public static void main(String[] args) {
-		ZigZag(4);
+		ZigZag(2, 30);
 	}
 
-	public static void ZigZag(int n) {
+	public static void ZigZag(int n, int m) {
 
 		for (int a = 0; a < n; a++) {
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < m; i++) {
 				System.out.print("|");
 				for (int j = 0; j < i; j++) {
 					System.out.print(" ");
 				}
 				System.out.print("*");
-				for (int j = i; j < 4; j++) {
+				for (int j = i; j < m-1; j++) {
 					System.out.print(" ");
 				}
 				System.out.println("|");
 			}
 
-			for (int i = 1; i < 5; i++) {
+			for (int i = 1; i < m; i++) {
 				System.out.print("|");
-				for (int j = 4; j > i; j--) {
+				for (int j = m-1; j > i; j--) {
 					System.out.print(" ");
 				}
 				System.out.print("*");
